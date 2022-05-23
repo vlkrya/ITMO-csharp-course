8a2afbc2b9e2277952f67fc0a773181c6192d28c
Anastasia Pe <95353760+vlkrya@users.noreply.github.com>
2022-05-24 00:00:38 +0500

    Add files via upload

---
+++ Lab4/Starter/WhatDay1/Whatday1/Whatday1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ITMO.CSharpcourse
+{ 
+    enum MonthName { January, February, Match, April, May, June, July, August, September, October, November, December }
+    class WhatDay
+    {
+        static void Main()
+        {
+            Console.Write("Please enter a day number between 1 and 365: ");
+            string line = Console.ReadLine();
+            int dayNum = int.Parse(line);
+
+            int monthNum = 0;
+            int[] DaysInMonths = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            foreach (int daysInMonth in DaysInMonths)
+            {
+                if (dayNum <= daysInMonth)
+                {
+                    break;
+                }
+                else
+                {
+                    dayNum -= daysInMonth;
+                    monthNum++;
+                }
+            }
+            MonthName temp = (MonthName)monthNum;
+            string monthName = temp.ToString();
+
+            Console.WriteLine(" {0} {1} ", dayNum, monthName);
+
+
+
+        }
+    }
+}
