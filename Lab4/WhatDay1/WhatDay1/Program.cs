b9e18f3240c501ef4239d1edce5eb10374d231e6
Anastasia Pe <95353760+vlkrya@users.noreply.github.com>
2022-05-25 01:12:09 +0500

    Add files via upload

---
+++ Lab4/WhatDay1/WhatDay1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSharpCourse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a day number between 1 and 365: ");
+            string line = Console.ReadLine();
+            int dayNum = int.Parse(line);
+        }
+    }
+}
+
