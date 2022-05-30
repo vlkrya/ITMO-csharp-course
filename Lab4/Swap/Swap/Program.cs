9cd81766a640d18b3706fe800fc35755087618c1
Anastasia Pe <95353760+vlkrya@users.noreply.github.com>
2022-05-30 15:44:09 +0500

    Add files via upload

---
+++ Lab4/Swap/Swap/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSharpCourse
+{
+    public class Test
+
+    {
+        public static void Main()
+        {
+            int x; // Input value 1
+            int y; // Input value 2
+
+            Console.WriteLine("Enter first number:");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Before swap: " + x + "," + y);
+            Swap(ref x, ref y);
+            Console.WriteLine("After swap: " + x + "," + y);
+        }
+
+        private static void Swap(ref int a, ref int b)
+        {
+            
+
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
---
+++ Lab4/Utility/Utility/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSharpCourse
+{
+    class Utils
+    {
+        //
+        // Return the greater of two integer values
+        //
+
+        public static int Greater(int a, int b)
+        {
+            if (a > b)
+                return a;
+            else
+                return b;
+        }
+    }
+
+    /// <summary>
+    /// This the test harness
+    /// </summary>
+
+    public class Test
+
+    {
+        public static void Main()
+        {
+            int x; // Input value 1
+            int y; // Input value 2
+            int greater; // Result from Greater()
+
+            // Get input numbers
+            Console.WriteLine("Enter first number:");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            y = int.Parse(Console.ReadLine());
+
+            // Test the Greater( ) method
+            greater = Utils.Greater(x, y);
+            Console.WriteLine("The greater value is {}"+ greater);
+
+        }
+    
+
+    }
+}
