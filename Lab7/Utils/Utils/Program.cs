04c51e0b7c64399644806bb238e2c0a4b501bb43
Anastasia Pe <95353760+vlkrya@users.noreply.github.com>
2022-05-31 17:04:33 +0500

    Add files via upload

---
+++ Lab7/Utils/Utils/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    internal class Program
+    {
+        public static void Reverse(ref string s)
+        {
+            string sRev = "";
+
+            for (int k = s.Length - 1; k >= 0; k--)
+            {
+                sRev = sRev + s[k];
+            }
+            s = sRev;
+        }
+        static void Main()
+        {
+            string message;
+
+            // Get an input string
+            Console.WriteLine("Enter string to reverse:");
+            message = Console.ReadLine();
+
+            // Reverse the string
+            Reverse(ref message);
+
+            // Display the result
+            Console.WriteLine(message);
+
+            Console.ReadLine();
+        }
+    }
+}
